54799e40cb6efdf4b56d7a8c00679b371b4c8a51
Данило Кордюк <KordykDanya@outlook.com>
2024-09-13 21:36:57 -0400

    sending co

---
+++ dz_001/dz_001.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "\tAll of us, we tell the travelers\n\tIsn't your endless ambition rotten?\n\tThe world is intertwined with legends\n\tthat need to be destroyed.\n\tEven now, All of us, we are waiting\n\tIt's hard to read.\n\n\tHiroshi Kitadani.";
+
+    std::cout << "\n\n\n\tEvery\n\t\t\hunter\n\t\t\twants\n\t\t\t\tto\n\t\t\t\t\tknow\n\t\t\t\t\t\twhere\n\t\t\t\t\t\t\tthe\n\t\t\t\t\t\t\t\tpheasant\n\t\t\t\t\t\t\t\t\t\tis.";
+
+    std::cout << "\n\n\nInsert card.\nEnter PIN.\nPIN verification.\nIf incorrect PIN - error, re - enter PIN.\nIf correct PIN - access granted.\nChoose operation  \n\t1.Withdraw\n\t2.Deposit\n\t3.Check balance\nPerforming the operation.\nTake your card.\n";
+
+    std::cout << "\n\n\nPreparing to open the library.\nOpen the doors.\nCheck equipment functionality.\nPrepare books and materials for lending.\nAllow visitors to enter.\nLibrary operations have started.";
+
+    std::cout << "\n\n\nFor Sale: GPU.\nVersion: NEW RTX4080S.\nPrice: 1450 CA.\nPhone: 849-16-43";
+
+    return 0;
+}
